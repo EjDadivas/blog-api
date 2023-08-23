1990598a1d9675e6be965fcbfc080a4326ad66ad
ej <99721216+EjDadivas@users.noreply.github.com>
2023-08-23 16:38:03 +0800

    created models and routes

---
+++ models/user.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  role: {
+    type: String,
+    required: true,
+    default: "User",
+  },
+});
+
+module.exports = mongoose.model("User", userSchema);
